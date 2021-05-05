b3d680e0e2b5cab129f360392bc0778a009a4319
Abdur Rehman <arehman@xiqinc.com>
2021-05-05 22:29:43 +0500

    adding apaache wsgi file

--- dellSSO/wsgi.py
+++ dellSSO/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for dellSSO project.
+#!/usr/bin/env python3
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
+import django
 import os
+import sys
+sys.path.append('/home/ubuntu/saml/ssopoc')
+sys.path.append('/home/ubuntu/saml/ssopoc/dellSSO')
 
-from django.core.wsgi import get_wsgi_application
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_server'
+django.setup()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dellSSO.settings')
-
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
